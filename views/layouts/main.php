0036e06110e46af5d63f4d9f606e5ed2965f955d
nachokrom <nachokroma@gmail.com>
2023-12-07 11:00:41 +0200

    Исправление входа

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -47,7 +47,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
                 : '<li class="nav-item">'
                     . Html::beginForm(['/site/logout'])
                     . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->username . ')',
+                        'Logout (' . Yii::$app->user->identity->login . ')',
                         ['class' => 'nav-link btn btn-link logout']
                     )
                     . Html::endForm()
